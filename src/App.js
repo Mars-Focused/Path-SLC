2e792dd72dba08b7f946bf49d23cb0eaf8978df8
Mars_Clarke <marsthecoder@outlook.com>
2021-03-06 19:30:26 -0700

    added buttons to header, functionality yet to beimplemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Login from "./Components/AdminLogin/Login";
 import AdminCtrl from "./Components/AdminCtrl/AdminCtrl";
 import About from "./Components/About/About";
-import Header from "./Components/Header/Header"
+import Header from "./Components/Header/Header";
+import Events from "./Components/Events/Events";
+import Blog from "./Components/Blog/Blog";
+import Announcements from "./Components/Announcements/Announcements";
+import MailList from "./Components/MailList/MailList";
+
 import { Route, HashRouter, Switch, BrowserRouter } from "react-router-dom";
 const Router =
   process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
@@ -9,13 +14,16 @@ const Router =
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Router>
         <Switch>
           <Route path="/" exact component={About} />
           <Route path="/Admin" exact component={Login} />
           <Route path="/AdminCtrl" exact component={AdminCtrl} />
-          <Route />
+          <Route path="/Events" exact component={Events} />
+          <Route path="/Blog" exact component={Blog} />
+          <Route path="/Announcements" exact component={Announcements} />
+          <Route path="/MailList" exact component={MailList} />
         </Switch>
       </Router>
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,35 @@
 import React from "react";
 import "./_Header.scss";
+import pathLogo from "../../images/path-logo-smol.png";
 
-export default function Header() {
-  return <div className="header">Header</div>;
+export default function Header(props) {
+  return (
+    <div className="header">
+      <img src={pathLogo} alt="" />
+      <button
+        className="HeaderLink"
+        onClick={() => props.history.push("/Events")}
+      >
+        EVENTS
+      </button>
+      <button
+        className="HeaderLink"
+        onClick={() => props.history.push("/Blog")}
+      >
+        BLOG
+      </button>
+      <button
+        className="HeaderLink"
+        onClick={() => props.history.push("/Announcements")}
+      >
+        ANNOUNCEMENTS
+      </button>
+      <button
+        className="HeaderLink"
+        onClick={() => props.history.push("/MailList")}
+      >
+        MAILING LIST
+      </button>
+    </div>
+  );
 }
