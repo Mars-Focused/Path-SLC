85205dc4b629e6ec786d6a7af25150efad3209ce
Mars_Clarke <marsthecoder@outlook.com>
2021-03-10 08:22:46 -0700

    moved router to index rather than into app

--- src/App.js
+++ src/App.js
@@ -7,26 +7,22 @@ import Blog from "./Components/Blog/Blog";
 import Announcements from "./Components/Announcements/Announcements";
 import MailList from "./Components/MailList/MailList";
 import AgeVerify from "./Components/AgeVerify/AgeVerify";
-
-import { Route, HashRouter, Switch, BrowserRouter } from "react-router-dom";
-const Router =
-  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={AgeVerify} />
-          <Route path="/Admin" exact component={Login} />
-          <Route path="/AdminCtrl" exact component={AdminCtrl} />
-          <Route path="/Events" exact component={Events} />
-          <Route path="/Blog" exact component={Blog} />
-          <Route path="/Announcements" exact component={Announcements} />
-          <Route path="/MailList" exact component={MailList} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={AgeVerify} />
+        <Route path="/About" exact component={About} />
+        <Route path="/Admin" exact component={Login} />
+        <Route path="/AdminCtrl" exact component={AdminCtrl} />
+        <Route path="/Events" exact component={Events} />
+        <Route path="/Blog" exact component={Blog} />
+        <Route path="/Announcements" exact component={Announcements} />
+        <Route path="/MailList" exact component={MailList} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+const Router =
+  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
+
 // import "./main.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
