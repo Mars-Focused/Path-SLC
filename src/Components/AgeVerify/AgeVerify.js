edd6bc5c23f18fe205fe5f15efa3f5f36c886fa7
Mars_Clarke <marsthecoder@outlook.com>
2021-03-21 18:49:52 -0600

    added Yes/No buttons to the AgeVerify component

--- src/Components/AgeVerify/AgeVerify.js
+++ src/Components/AgeVerify/AgeVerify.js
@@ -1,5 +1,21 @@
+import "./_AgeVerify.scss";
 import React from "react";
+import { withRouter } from "react-router";
 
-export default function AgeVerify() {
-  return <div className="age-verify">AgeVerify</div>;
-}
+const AgeVerify = (props) => {
+  return (
+    <div className="age-verify">
+      <a className="verify-btn" href="https://www.google.com/">
+        No
+      </a>
+      <button
+        className="verify-btn"
+        onClick={() => props.history.push("/About")}
+      >
+        Yes
+      </button>
+    </div>
+  );
+};
+
+export default withRouter(AgeVerify);
