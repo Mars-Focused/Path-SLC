79797dca94ca9333dfc0924da35c26b4d23ac1a2
Mars_Clarke <marsthecoder@outlook.com>
2021-03-22 14:30:12 -0600

    improved AgeVerify Component

--- src/Components/AgeVerify/AgeVerify.js
+++ src/Components/AgeVerify/AgeVerify.js
@@ -5,15 +5,19 @@ import { withRouter } from "react-router";
 const AgeVerify = (props) => {
   return (
     <div className="age-verify">
-      <a className="verify-btn" href="https://www.google.com/">
-        No
-      </a>
-      <button
-        className="verify-btn"
-        onClick={() => props.history.push("/About")}
-      >
-        Yes
-      </button>
+      <div>You must be at least 18 Years old in order to enter</div>
+      <div>Are you 18 years old or older?</div>
+      <div className="verify-btns">
+        <a className="verify-btn" href="https://www.google.com/">
+          No
+        </a>
+        <button
+          className="verify-btn"
+          onClick={() => props.history.push("/About")}
+        >
+          Yes
+        </button>
+      </div>
     </div>
   );
 };
